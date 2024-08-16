0780dc6be55933edec6bf0f98120c1d532305063
Jay25102 <jasonhuang25102@gmail.com>
2024-08-16 14:45:17 -0400

    insertion

--- insertion.js
+++ insertion.js
@@ -1,5 +1,15 @@
-function insertionSort(
-    
-) {}
+function insertionSort(inputArr) {
+    for (let i = 0; i < inputArr.length; i++) {
+        let currValue = inputArr[i];
+
+        for (var j = i - 1; j > -1 && inputArr[j] > currValue; j--) {
+            inputArr[j + 1] = inputArr[j];
+        }
+
+        inputArr[j + 1] = currValue;
+    }
+
+    return inputArr;
+}
 
 module.exports = insertionSort;
